f0c6d44c7b2d110299b0f1af1d7d295468af8509
Freda1120 <74005510+Freda1120@users.noreply.github.com>
2022-02-19 16:21:03 -0800

    "Quiz 1"

--- quiz/src/ui/yinwen/Main.java
+++ quiz/src/ui/yinwen/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Student st1 = new Student("aa", 12, 222);
+        st1.takeQuiz("q1");
+        Professor p1 = new Professor("ab", 72, 222);
+        st1.takeQuiz("q1");
     }
 }
