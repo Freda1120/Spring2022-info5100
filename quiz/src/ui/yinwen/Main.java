2983d93382077e88ea728a81593268d1abbb99b2
Freda1120 <74005510+Freda1120@users.noreply.github.com>
2022-02-19 16:21:51 -0800

    "Quiz 1"

--- quiz/src/ui/yinwen/Main.java
+++ quiz/src/ui/yinwen/Main.java
@@ -7,6 +7,6 @@ public class Main {
         Student st1 = new Student("aa", 12, 222);
         st1.takeQuiz("q1");
         Professor p1 = new Professor("ab", 72, 222);
-        st1.takeQuiz("q1");
+        p1.giveQuiz("q1");
     }
 }
