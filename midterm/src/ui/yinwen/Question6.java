f1ef46e16110e152e666e11976c952b6b74d01ad
Freda1120 <74005510+Freda1120@users.noreply.github.com>
2022-03-26 15:44:55 -0700

    "Midterm"

--- midterm/src/ui/yinwen/Question6.java
+++ midterm/src/ui/yinwen/Question6.java
@@ -4,19 +4,17 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Question6 {
-    public int lengthOfLongestSubstring(String s) {
-        int i = 0, j = 0, max = 0;
+    public int getLongest(String s) {
+        int i = 0, j = 0, res = 0;
         Set<Character> set = new HashSet<>();
-
         while (j < s.length()) {
             if (!set.contains(s.charAt(j))) {
                 set.add(s.charAt(j++));
-                max = Math.max(max, set.size());
+                res = Math.max(res, set.size());
             } else {
                 set.remove(s.charAt(i++));
             }
         }
-
-        return max;
+        return res;
     }
 }
