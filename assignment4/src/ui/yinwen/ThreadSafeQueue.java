1da01784a3b67f8186819fda408285d4b50c638f
Freda1120 <74005510+Freda1120@users.noreply.github.com>
2022-03-29 11:57:56 -0700

    "Assignment4"

---
+++ assignment4/src/ui/yinwen/ThreadSafeQueue.java
@@ -0,0 +1,36 @@
+package ui.yinwen;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ThreadSafeQueue<T> {
+    private Queue<T> safeQueue = new LinkedList<>();
+
+
+    public void add(T data){
+        synchronized (safeQueue){
+            this.safeQueue.add(data);
+        }
+    }
+
+    public T remove(){
+        synchronized (safeQueue){
+            return this.safeQueue.remove();
+        }
+    }
+
+    public T peek(){
+        synchronized (safeQueue){
+            return this.safeQueue.peek();
+        }
+    }
+
+    public boolean isEmpty(){
+        synchronized (safeQueue){
+            return this.safeQueue.peek() == null;
+        }
+    }
+
+
+
+}
